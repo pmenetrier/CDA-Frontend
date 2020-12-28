3c1d26f4735f15deeded61247c4e0d25b499f0db
laurent.aubry <laurent.aubry@andre-legroupe.com>
2020-12-28 09:27:15 +0100

    Création du composant Card.js et de son css

---
+++ src/card/card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './card.css'
+
+const Card = () => {
+
+    return (
+        <div>
+            <h1>Mon élément</h1>
+        </div>
+    )
+}
+
+export default Card
