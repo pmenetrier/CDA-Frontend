28b4138e57a7d5fdcf6a1b7a046820af89348b2f
laurent.aubry <laurent.aubry@andre-legroupe.com>
2020-12-28 12:03:29 +0100

    CSS et composant du Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Musiques from './pages/musiques'
 import Films from './pages/films'
+import Header from './header/header'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Bienvenue au GRETA</h1>
       <Musiques />
       <br/>
---
+++ src/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="main-header">
+        <h1> mon header</h1>
+        </div>
+    )
+}
+
+export default Header
